1964294b71aa3468677c30b0b5c78707e49b87d7
negralessio <Alessio.Negrini@live.de>
2023-06-12 14:41:14 +0200

    Added load_datasets.py from supervisor

---
+++ src/load_datasets.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+from pathlib import Path
+from typing import Union
+
+
+def load_dataset(path: Union[Path, str]) -> pd.DataFrame:
+    return pd.read_csv(path)
+
+
+def load_rankings(path: Union[Path, str]) -> pd.DataFrame:
+    out = pd.read_csv(path, index_col=0, header=[0, 1, 2, 3])
+    out.columns.name = ("dataset", "model", "tuning", "scoring")
+    return out
+
