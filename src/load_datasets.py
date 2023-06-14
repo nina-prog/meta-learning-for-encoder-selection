2961e6bf51c0295b84b7f5c9c08c816be83bc73e
negralessio <Alessio.Negrini@live.de>
2023-06-14 10:32:04 +0200

    Added verbosity param

--- src/load_datasets.py
+++ src/load_datasets.py
@@ -4,11 +4,13 @@ from pathlib import Path
 from typing import Union
 
 
-def load_dataset(path: Union[Path, str]) -> pd.DataFrame:
+def load_dataset(path: Union[Path, str], verbosity=1) -> pd.DataFrame:
+    if verbosity > 0: print("Loading data ...")
     return pd.read_csv(path, index_col=0)
 
 
-def load_rankings(path: Union[Path, str]) -> pd.DataFrame:
+def load_rankings(path: Union[Path, str], verbosity=1) -> pd.DataFrame:
+    if verbosity > 0: print("Loading rankings ...")
     out = pd.read_csv(path, index_col=0, header=[0, 1, 2, 3])
     out.columns.name = ("dataset", "model", "tuning", "scoring")
     return out
