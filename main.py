5cd347eaf6a9c19f10bd5805ddd7db886f3279e5
negralessio <Alessio.Negrini@live.de>
2023-06-14 11:22:52 +0200

    Implemented mlflow that now tracks our pipeline runs

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ Module for the pipeline. Execute via python3 main.py --config configs/config.yam
 """
 import time
 
+import mlflow.sklearn
+
 import src.utils
 import src.load_datasets
 import src.modelling
+import src.mlflow_registry
 
 
 def main():
@@ -20,7 +23,7 @@ def main():
     print(20 * "=" + " Starting pipeline " + 20 * "=")
 
     # Read config file
-    cfg = src.utils.parse_args()
+    cfg, cfg_path = src.utils.parse_args()
     verbosity = cfg["general"]["verbosity"]
 
     # Load Data
@@ -34,14 +37,20 @@ def main():
     # Split data into train test
     X_train, X_test, y_train, y_test = src.modelling.train_test_split_data(train_data=dataset,
                                                                            split_size=cfg["modelling"]["split_size"])
-    # Perform CV and train model
-    model, cv_result = src.modelling.train_model(model=cfg["modelling"]["model"],
-                                                 train_data=X_train,
-                                                 train_labels=y_train,
-                                                 scoring=cfg["modelling"]["scoring"],
-                                                 hyperparam_grid=None,
-                                                 verbosity=verbosity,
-                                                 k_fold=cfg["modelling"]["k_fold"])
+
+    # Log model evaluation to mlflow registry
+    mlflow.sklearn.autolog(log_models=False)
+    with mlflow.start_run(tags = {"train_set_size": len(X_train)}):
+        # Perform CV and train model
+        model, cv_result = src.modelling.train_model(model=cfg["modelling"]["model"],
+                                                     train_data=X_train,
+                                                     train_labels=y_train,
+                                                     scoring=cfg["modelling"]["scoring"],
+                                                     hyperparam_grid=None,
+                                                     verbosity=verbosity,
+                                                     k_fold=cfg["modelling"]["k_fold"])
+        src.mlflow_registry.log_model_eval(cv_result, cfg, cfg_path)
+
     # Make predictions
     src.modelling.make_prediction(model=model, test_data=X_test,
                                   result_path=cfg["paths"]["result_path"], verbosity=verbosity)
