8686cc08f9d7c1a2edf041797bbef61fbf670921
negralessio <Alessio.Negrini@live.de>
2023-06-16 12:02:33 +0200

    Added functionality to take subsample based on the parameter in args

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 """
-Module for the pipeline. Execute via python3 main.py --config configs/config.yaml
+Module for the pipeline.
+Execute via python3 main.py --config configs/config.yaml
+or if you want to run pipeline with subsample of size 100
+python3 main.py --config configs/config.yaml --subsample 100
 """
 import time
 
@@ -20,15 +23,20 @@ def main():
     # Parse config file
     # Track time for execution
     start_time = time.time()
-    print(20 * "=" + " Starting pipeline " + 20 * "=")
+    print(20 * "=" + " PIPELINE STARTED " + 20 * "=")
 
-    # Read config file
-    cfg, cfg_path = src.utils.parse_args()
+    # Parse arguments and read config file cfg
+    args = src.utils.parse_args()
+    cfg, cfg_path = src.utils.parse_config(args)
     verbosity = cfg["general"]["verbosity"]
 
     # Load Data
-    dataset = src.load_datasets.load_dataset(path=cfg["paths"]["dataset_path"], verbosity=verbosity)
-    rankings = src.load_datasets.load_rankings(path=cfg["paths"]["rankings_path"], verbosity=verbosity)
+    dataset = src.load_datasets.load_dataset(path=cfg["paths"]["dataset_path"],
+                                             verbosity=verbosity,
+                                             subsample=args.subsample)
+    rankings = src.load_datasets.load_rankings(path=cfg["paths"]["rankings_path"],
+                                               verbosity=verbosity,
+                                               subsample=args.subsample)
 
     """
     Add here pipeline steps, e.g. preprocessing, fitting, predictions ...
@@ -57,7 +65,7 @@ def main():
 
     # Track time for total runtime and display end of pipeline
     runtime = time.time() - start_time
-    print(20 * "=" + f" Pipeline finished ({src.utils.display_runtime(runtime)}) " + 20 * "=")
+    print(20 * "=" + f" PIPELINE FINISHED ({src.utils.display_runtime(runtime)}) " + 20 * "=")
 
 
 if __name__ == "__main__":
