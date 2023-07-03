fd3e34777636636d53d446586589a8cea52e7c75
negralessio <Alessio.Negrini@live.de>
2023-07-03 16:44:56 +0200

    Deleted dependency on cfg file; Now mlflow always tracks spearman, r2 and neg_MSE

--- src/mlflow_registry.py
+++ src/mlflow_registry.py
@@ -6,12 +6,11 @@ import mlflow
 import pandas as pd
 
 
-def log_model_eval(cv_results, cfg, cfg_path, run, verbosity=1):
+def log_model_eval(cv_results, cfg_path, run, verbosity=1):
     """
     Function to log different artefacts and metrics to the active mlflow run
 
     :param cv_results: pd.DataFrame -- CV results
-    :param cfg: dict -- Parsed config file
     :param cfg_path: str -- String to the config file
     :param run: ActiveRun mlflow instance
     :param verbosity: int -- Level of verbosity
@@ -26,7 +25,7 @@ def log_model_eval(cv_results, cfg, cfg_path, run, verbosity=1):
     mlflow.log_artifact(local_path=cfg_path)
 
     # Log scores
-    for scorer in cfg["modelling"]["scoring"]:
+    for scorer in ["spearman", "r2", "neg_mean_squared_error"]:
         mlflow.log_metric(key=f"cv_test_mean_{scorer}", value=round(cv_results[f'test_{scorer}'].mean(), 4))
         mlflow.log_metric(key=f"cv_test_std_{scorer}", value=round(cv_results[f'test_{scorer}'].std(), 4))
 
